bef16bc3258016460cf8a11a5fa50460c430c979
Takahito Nakano <kano@ponko2.jp>
2014-06-28 03:34:45 +0900

    historyの設定を調整

--- home/.oh-my-zsh-custom/history.zsh
+++ home/.oh-my-zsh-custom/history.zsh
@@ -7,5 +7,17 @@ fi
 HISTSIZE=10000
 SAVEHIST=10000
 
-# history (fc -l) no store
+# 履歴検索で重複しているものを表示しない
+setopt hist_find_no_dups
+
+# 同じコマンドが history に存在する場合は古い履歴を削除
+setopt hist_ignore_all_dups
+
+# 関数定義を履歴に残さない
+setopt hist_no_functions
+
+# history (fc -l) コマンドを履歴から削除
 setopt hist_no_store
+
+# コマンドの余計な空白を取り除いてから履歴に残す
+setopt hist_reduce_blanks
