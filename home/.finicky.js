e7bfffc350c9a16502f56676abb6e49c941effc2
Takahito Nakano <kano@ponko2.jp>
2016-05-21 22:46:52 +0900

    Finickyの設定を調整

--- home/.finicky.js
+++ home/.finicky.js
@@ -3,10 +3,24 @@ finicky.setDefaultBrowser('com.google.Chrome');
 finicky.onUrl(function (url, opts) {
   var sourceApplication = opts && opts.sourceBundleIdentifier;
 
+  var chrome = {
+    bundleIdentifier: 'com.google.Chrome'
+  };
+
   var firefox = {
     bundleIdentifier: 'org.mozilla.firefox',
     openInBackground: true
   };
+
+  // SNS
+  if (url.match(/^https?:\/\/twitter\.com/)) {
+    return chrome;
+  }
+
+  // Tech
+  if (url.match(/^https?:\/\/(github|qiita)\.com/)) {
+    return chrome;
+  }
 
   // Reeder
   if (sourceApplication === 'com.reederapp.rkit2.mac') {
