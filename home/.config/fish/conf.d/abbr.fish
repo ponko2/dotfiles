8cd8b9b04479f983d15502275aa8c36ca7875ce1
Takahito Nakano <kano@ponko2.jp>
2016-11-03 20:52:01 +0900

    fishの設定を調整

--- home/.config/fish/conf.d/abbr.fish
+++ home/.config/fish/conf.d/abbr.fish
@@ -1,6 +1,7 @@
 # Git
 abbr -a ga git add
 abbr -a gaa git add --all
+abbr -a gb git branch
 abbr -a gc git commit -v
 abbr -a gc! git commit -v --amend
 abbr -a gcn! git commit -v --amend --no-edit
@@ -10,5 +11,6 @@ abbr -a gdc git diff --cached
 abbr -a gl git log --decorate
 abbr -a glg git log --decorate --graph
 abbr -a glo git log --decorate --oneline
+abbr -a gm git merge
 abbr -a gs git status
 abbr -a gss git status -s
