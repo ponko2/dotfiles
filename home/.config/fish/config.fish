eb082e2239c1d34603d70ca4810b42693dc5d38e
Takahito Nakano <kano@ponko2.jp>
2016-11-05 00:58:59 +0900

    fishの設定を調整

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,17 +1,17 @@
 set -x LANG ja_JP.UTF-8
 set -x GITHUB_USER ponko2
 
-set -x PATH /usr/local/bin $PATH
-set -x PATH /usr/local/sbin $PATH
-set -x PATH /usr/local/var/nodebrew/current/bin $PATH
-set -x PATH $HOME/bin $PATH
-set -x PATH $HOME/.composer/vendor/bin $PATH
-set -x PATH $PATH ./node_modules/.bin
-
 set -x EDITOR vim
 set -x GOPATH $HOME
 set -x GPG_TTY (tty)
-set -x HOMEBREW_CASK_OPTS '--appdir=/Applications'
 set -x NODEBREW_ROOT /usr/local/var/nodebrew
-set -x NOTIFY_COMMAND_COMPLETE_TIMEOUT 10
 set -x RBENV_ROOT /usr/local/var/rbenv
+set -x RBENV_SHELL fish
+
+set -x PATH $HOME/.composer/vendor/bin $PATH
+set -x PATH $RBENV_ROOT/shims $PATH
+set -x PATH $NODEBREW_ROOT/current/bin $PATH
+set -x PATH ./node_modules/.bin $PATH
+set -x PATH $GOPATH/bin $PATH
+set -x PATH /usr/local/sbin $PATH
+set -x PATH /usr/local/bin $PATH
