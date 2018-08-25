1d4e1d7752722ac5d8a106d5f00a63e59cfa98a8
Takahito Nakano <kano@ponko2.jp>
2018-08-25 16:08:01 +0900

    Zshの設定を調整

--- home/.config/zplug/init.zsh
+++ home/.config/zplug/init.zsh
@@ -7,13 +7,5 @@ export ZPLUG_BIN=$XDG_DATA_HOME/zplug/bin
 fpath=($XDG_CONFIG_HOME/zplug/plugins/anyframe(N-/) $fpath)
 source $ZPLUG_HOME/init.zsh
 
-# # Install plugins if there are plugins that have not been installed
-# if ! zplug check --verbose; then
-#   printf "Install? [y/N]: "
-#   if read -q; then
-#     echo; zplug install
-#   fi
-# fi
-
 # Then, source plugins and add commands to $PATH
 zplug load # --verbose
--- home/.config/zplug/packages.zsh
+++ home/.config/zplug/packages.zsh
@@ -9,9 +9,6 @@ zplug 'sindresorhus/pure', use:pure.zsh, from:github, as:theme
 zplug 'b4b4r07/enhancd', use:init.sh
 zplug 'mollifier/anyframe'
 zplug 'mollifier/cd-gitroot'
-#zplug 'zsh-users/zsh-autosuggestions'
-zplug 'zsh-users/zsh-completions'
-#zplug 'zsh-users/zsh-history-substring-search'
 zplug 'zsh-users/zsh-syntax-highlighting', defer:2
 
 # local plugins
