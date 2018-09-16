4fd7ea69854f9ad2cd92314add1c58be3cd9f862
Takahito Nakano <kano@ponko2.jp>
2018-09-17 01:53:54 +0900

    Zshの設定を調整

--- home/.config/zplug/custom/completion.zsh
+++ home/.config/zplug/custom/completion.zsh
@@ -28,3 +28,5 @@ bindkey -M menuselect '^h' vi-backward-char
 bindkey -M menuselect '^j' vi-down-line-or-history
 bindkey -M menuselect '^k' vi-up-line-or-history
 bindkey -M menuselect '^l' vi-forward-char
+bindkey -M menuselect '^n' vi-down-line-or-history
+bindkey -M menuselect '^p' vi-up-line-or-history
