c6a00e3bfb3b9e3d5cf379e7bb33c75dab3525d5
Takahito Nakano <kano@ponko2.jp>
2018-10-21 18:02:07 +0900

    インストールスクリプトを調整

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash -u --
+#!/bin/bash -eux
 
 if [ -z "${SRCPATH:-}" ]; then
-    SRCPATH="$HOME/src/github.com/ponko2/dotfiles"
+  SRCPATH="$HOME/src/github.com/ponko2/dotfiles"
 fi
 
 if [ ! -d "$SRCPATH" ]; then
-  mkdir -p `dirname $SRCPATH` && cd $_
+  mkdir -p "$(dirname "$SRCPATH")" && cd "$_" || exit 1
   git clone https://github.com/ponko2/dotfiles.git
 fi
 
-cd $SRCPATH && make install
+cd "$SRCPATH" && make install
