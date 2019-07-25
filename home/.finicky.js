1054d57541bd096932e38f5853eb451d46af8cd8
Takahito Nakano <kano@ponko2.jp>
2019-07-25 22:59:04 +0900

    Finickyの設定を調整

--- home/.finicky.js
+++ home/.finicky.js
@@ -1,88 +1,48 @@
-finicky.setDefaultBrowser("org.mozilla.firefox");
-
-finicky.onUrl(function(url, opts) {
-  var sourceApplication = opts && opts.sourceBundleIdentifier;
-
-  // Reeder
-  if (sourceApplication === "com.reederapp.rkit2.mac") {
-    var chrome = {
-      bundleIdentifier: "com.google.Chrome",
-      openInBackground: true
-    };
-
-    // SNS
-    if (url.match(/^https?:\/\/twitter\.com/)) {
-      return chrome;
+module.exports = {
+  defaultBrowser: "Firefox",
+  handlers: [
+    {
+      match: options => {
+        if (options.sourceBundleIdentifier !== "com.reederapp.macOS") {
+          return false;
+        }
+        const domains = ["twitter.com", "github.com", "qiita.com"];
+        return finicky.matchDomains(domains)(options);
+      },
+      browser: {
+        name: "Google Chrome",
+        openInBackground: true
+      }
+    },
+    {
+      match: ({ sourceBundleIdentifier }) =>
+        sourceBundleIdentifier === "com.reederapp.macOS",
+      browser: {
+        name: "Firefox",
+        openInBackground: true
+      }
+    },
+    {
+      match: ({ sourceBundleIdentifier }) =>
+        [
+          // Slack
+          "com.tinyspeck.slackmacgap",
+          // 1Password
+          "com.agilebits.onepassword7",
+          // Thunderbird
+          "org.mozilla.thunderbird",
+          // Excel
+          "com.microsoft.Excel",
+          // Word
+          "com.microsoft.Word",
+          // PowerPoint
+          "com.microsoft.Powerpoint",
+          // iTerm2
+          "com.googlecode.iterm2",
+          // Command Line Tools
+          null
+        ].includes(sourceBundleIdentifier),
+      browser: "Google Chrome"
     }
-
-    // GitHub
-    if (url.match(/^https?:\/\/github/)) {
-      return chrome;
-    }
-
-    // Qiita
-    if (url.match(/^https?:\/\/qiita\.com/)) {
-      return chrome;
-    }
-
-    return {
-      openInBackground: true
-    };
-  }
-
-  // Slack
-  if (sourceApplication === "com.tinyspeck.slackmacgap") {
-    return {
-      bundleIdentifier: "com.google.Chrome"
-    };
-  }
-
-  // 1Password
-  if (sourceApplication === "com.agilebits.onepassword7") {
-    return {
-      bundleIdentifier: "com.google.Chrome"
-    };
-  }
-
-  // Thunderbird
-  if (sourceApplication === "org.mozilla.thunderbird") {
-    return {
-      bundleIdentifier: "com.google.Chrome"
-    };
-  }
-
-  // Excel
-  if (sourceApplication === "com.microsoft.Excel") {
-    return {
-      bundleIdentifier: "com.google.Chrome"
-    };
-  }
-
-  // Word
-  if (sourceApplication === "com.microsoft.Word") {
-    return {
-      bundleIdentifier: "com.google.Chrome"
-    };
-  }
-
-  // Powerpoint
-  if (sourceApplication === "com.microsoft.Powerpoint") {
-    return {
-      bundleIdentifier: "com.google.Chrome"
-    };
-  }
-
-  // iTerm2
-  if (sourceApplication === "com.googlecode.iterm2") {
-    return {
-      bundleIdentifier: "com.google.Chrome"
-    };
-  }
-
-  // Command Line Tools
-  if (sourceApplication === null) {
-    return {
-      bundleIdentifier: "com.google.Chrome"
-    };
-  }
-});
+  ]
+};
