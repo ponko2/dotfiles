6e022831f983302d7062a7070c69b05910e18772
Takahito Nakano <kano@ponko2.jp>
2021-05-02 14:26:13 +0900

    Zshの設定を調整

--- home/.config/zplug/init.zsh
+++ home/.config/zplug/init.zsh
@@ -4,7 +4,6 @@ export ZPLUG_LOADFILE=$XDG_CONFIG_HOME/zplug/packages.zsh
 export ZPLUG_CACHE_DIR=$XDG_CACHE_HOME/zplug
 export ZPLUG_REPOS=$XDG_DATA_HOME/zplug/repos
 export ZPLUG_BIN=$XDG_DATA_HOME/zplug/bin
-fpath=($XDG_CONFIG_HOME/zplug/plugins/anyframe(N-/) $fpath)
 source $ZPLUG_HOME/init.zsh
 
 # Then, source plugins and add commands to $PATH
--- home/.zshrc
+++ home/.zshrc
@@ -4,6 +4,8 @@ if [ "$TERM" != 'dumb' ]; then
   stty -ixon -ixoff
 fi
 
+fpath=($XDG_CONFIG_HOME/anyframe(N-/) $fpath)
+
 if [[ -f /usr/local/opt/zplug/init.zsh ]]; then
   source $XDG_CONFIG_HOME/zplug/init.zsh
 fi
