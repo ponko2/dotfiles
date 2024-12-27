7e3af52d08fad3e6fb1b841aa7f4707f00b0fa50
Takahito Nakano <kano@ponko2.jp>
2024-12-27 22:00:15 +0900

    Zshの設定を調整

--- home/.zshrc.d/aliases.zsh
+++ home/.zshrc.d/aliases.zsh
@@ -61,25 +61,5 @@ if command -v colordiff >/dev/null; then
   alias diff='colordiff'
 fi
 
-# Git
-alias ga='git add'
-alias gaa='git add --all'
-alias gb='git branch'
-alias gc='git commit -v'
-alias gc!='git commit -v --amend'
-alias gcn!='git commit -v --amend --no-edit'
-alias gco='git checkout'
-alias gd='git diff'
-alias gdc='git diff --cached'
-alias gl='git log --decorate'
-alias glg='git log --decorate --graph'
-alias glo='git log --decorate --oneline'
-alias gm='git merge'
-alias gr='git restore'
-alias gs='git status'
-alias gss='git status -s'
-alias gsw='git switch'
-alias gt='cd-gitroot'
-
 # Vim
 alias vi='vim'
