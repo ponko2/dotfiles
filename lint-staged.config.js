f0b7452284d2d8996221da08ad10f5ea7e0bf84c
Takahito Nakano <kano@ponko2.jp>
2025-06-22 00:32:43 +0900

    CIの設定を調整

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,7 @@
  */
 export default {
   "*.lua": ["luacheck", "stylua"],
-  "*.{cjs,cts,js,json,jsonc,jsx,mjs,mts,ts,tsx}": "biome check --write",
+  "*.{cjs,cts,js,json,jsonc,jsx,mjs,mts,ts,tsx}":
+    "biome check --write --files-ignore-unknown=true --no-errors-on-unmatched",
   "*.{yaml,yml}": "yamllint",
 };
