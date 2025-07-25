44b0ddde9d8317f7f7be5ef9aff6bea43d484391
Takahito Nakano <kano@ponko2.jp>
2025-07-25 22:55:14 +0900

    Neovimの設定を調整

--- home/.config/nvim/lua/plugins/motion.lua
+++ home/.config/nvim/lua/plugins/motion.lua
@@ -29,6 +29,138 @@ return {
     end,
   },
   {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      branch = 'main',
+    },
+    keys = {
+      {
+        'af',
+        function()
+          require('nvim-treesitter-textobjects.select').select_textobject(
+            '@function.outer',
+            'textobjects'
+          )
+        end,
+        mode = { 'x', 'o' },
+      },
+      {
+        'if',
+        function()
+          require('nvim-treesitter-textobjects.select').select_textobject(
+            '@function.inner',
+            'textobjects'
+          )
+        end,
+        mode = { 'x', 'o' },
+      },
+      {
+        'ac',
+        function()
+          require('nvim-treesitter-textobjects.select').select_textobject(
+            '@class.outer',
+            'textobjects'
+          )
+        end,
+        mode = { 'x', 'o' },
+      },
+      {
+        'ic',
+        function()
+          require('nvim-treesitter-textobjects.select').select_textobject(
+            '@class.inner',
+            'textobjects'
+          )
+        end,
+        mode = { 'x', 'o' },
+      },
+      {
+        ']m',
+        function()
+          require('nvim-treesitter-textobjects.move').goto_next_start(
+            '@function.outer',
+            'textobjects'
+          )
+        end,
+        mode = { 'n', 'x', 'o' },
+      },
+      {
+        ']]',
+        function()
+          require('nvim-treesitter-textobjects.move').goto_next_start('@class.outer', 'textobjects')
+        end,
+        mode = { 'n', 'x', 'o' },
+      },
+      {
+        ']M',
+        function()
+          require('nvim-treesitter-textobjects.move').goto_next_end(
+            '@function.outer',
+            'textobjects'
+          )
+        end,
+        mode = { 'n', 'x', 'o' },
+      },
+      {
+        '][',
+        function()
+          require('nvim-treesitter-textobjects.move').goto_next_end('@class.outer', 'textobjects')
+        end,
+        mode = { 'n', 'x', 'o' },
+      },
+      {
+        '[m',
+        function()
+          require('nvim-treesitter-textobjects.move').goto_previous_start(
+            '@function.outer',
+            'textobjects'
+          )
+        end,
+        mode = { 'n', 'x', 'o' },
+      },
+      {
+        '[[',
+        function()
+          require('nvim-treesitter-textobjects.move').goto_previous_start(
+            '@class.outer',
+            'textobjects'
+          )
+        end,
+        mode = { 'n', 'x', 'o' },
+      },
+      {
+        '[M',
+        function()
+          require('nvim-treesitter-textobjects.move').goto_previous_end(
+            '@function.outer',
+            'textobjects'
+          )
+        end,
+        mode = { 'n', 'x', 'o' },
+      },
+      {
+        '[]',
+        function()
+          require('nvim-treesitter-textobjects.move').goto_previous_end(
+            '@class.outer',
+            'textobjects'
+          )
+        end,
+        mode = { 'n', 'x', 'o' },
+      },
+    },
+    opts = {
+      select = {
+        lookahead = true,
+      },
+      move = {
+        set_jumps = true,
+      },
+    },
+  },
+  {
     'rhysd/accelerated-jk',
     keys = {
       { 'j', [[<Plug>(accelerated_jk_gj)]] },
--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -2,73 +2,34 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     branch = 'main',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-      branch = 'main',
-    },
     event = { 'BufReadPost', 'BufNewFile', 'BufWritePre', 'VeryLazy' },
     build = ':TSUpdate',
-    opts = {
-      -- A list of parser names, or "all"
-      ensure_installed = 'all',
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      auto_install = false,
-
-      -- List of parsers to ignore installing (for "all")
-      ignore_install = {},
-
-      highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-
-        -- list of language that will be disabled
-        disable = {},
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-          },
+    config = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = {
+          'css',
+          'go',
+          'html',
+          'javascript',
+          'json',
+          'lua',
+          'markdown',
+          'python',
+          'scss',
+          'toml',
+          'typescript',
+          'vim',
+          'vue',
         },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          },
-        },
-      },
-    },
+        callback = function()
+          -- syntax highlighting, provided by Neovim
+          vim.treesitter.start()
+          -- folds, provided by Neovim
+          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          -- indentation, provided by nvim-treesitter
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
   },
 }
