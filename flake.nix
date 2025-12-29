4e8bf054589e5afcba278c3c677235dc7f0afe7c
Takahito Nakano <kano@ponko2.jp>
2025-12-30 04:30:00 +0900

    Nixの設定を調整

--- flake.nix
+++ flake.nix
@@ -87,6 +87,7 @@
               {
                 home-manager = {
                   useGlobalPkgs = true;
+                  useUserPackages = true;
                   users.${user.name} = ./home.nix;
                 };
               }
