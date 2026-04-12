bd1825c1f6314167057d2d9f95468a933f54e8ef
Takahito Nakano <kano@ponko2.jp>
2026-04-12 20:08:53 +0900

    feat: increase UI contrast for line numbers

--- home/.config/nvim/lua/plugins/syntax.lua
+++ home/.config/nvim/lua/plugins/syntax.lua
@@ -7,6 +7,7 @@ return {
     init = function()
       vim.g.gruvbox_material_foreground = 'original'
       vim.g.gruvbox_material_transparent_background = 1
+      vim.g.gruvbox_material_ui_contrast = 'high'
       vim.g.gruvbox_material_visual = 'reverse'
     end,
     config = function()
