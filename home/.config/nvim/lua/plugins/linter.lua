5b8fa00516d28f0eafb2bbcb82076842da3a6151
Takahito Nakano <kano@ponko2.jp>
2026-04-19 21:30:00 +0900

    chore(neovim): update lint settings

--- home/.config/nvim/lua/plugins/linter.lua
+++ home/.config/nvim/lua/plugins/linter.lua
@@ -6,24 +6,24 @@ return {
     init = function()
       vim.g.ale_echo_msg_format = '[%linter%] %code: %%s [%severity%]'
       vim.g.ale_fixers = {
-        css = { 'biome', 'prettier' },
-        javascript = { 'biome', 'eslint', 'prettier' },
-        javascriptreact = { 'biome', 'eslint', 'prettier' },
+        css = { 'prettier' },
+        javascript = { 'eslint', 'prettier' },
+        javascriptreact = { 'eslint', 'prettier' },
         markdown = { 'textlint' },
         text = { 'textlint' },
-        typescript = { 'biome', 'eslint', 'prettier' },
-        typescriptreact = { 'biome', 'eslint', 'prettier' },
+        typescript = { 'eslint', 'prettier' },
+        typescriptreact = { 'eslint', 'prettier' },
       }
       vim.g.ale_fix_on_save = 1
       vim.g.ale_linters = {
         go = { 'golangci-lint' },
-        javascript = { 'biome', 'eslint' },
-        javascriptreact = { 'biome', 'eslint' },
+        javascript = { 'eslint' },
+        javascriptreact = { 'eslint' },
         lua = { 'luacheck' },
         markdown = { 'textlint' },
         text = { 'textlint' },
-        typescript = { 'biome', 'eslint' },
-        typescriptreact = { 'biome', 'eslint' },
+        typescript = { 'eslint' },
+        typescriptreact = { 'eslint' },
       }
       vim.g.ale_linters_explicit = 1
       vim.g.ale_sign_column_always = 1
