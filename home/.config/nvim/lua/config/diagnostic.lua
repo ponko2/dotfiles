923dac225fc060e097eaaa961ff610d67a4e7d6c
Takahito Nakano <kano@ponko2.jp>
2026-05-02 02:28:59 +0900

    chore(neovim): update diagnostic settings

--- home/.config/nvim/lua/config/diagnostic.lua
+++ home/.config/nvim/lua/config/diagnostic.lua
@@ -3,13 +3,34 @@
 --------------------------------------------------------------------------------
 
 vim.diagnostic.handlers.loclist = {
+  ---@param bufnr integer
   ---@param opts { loclist: vim.diagnostic.setloclist.Opts }
-  show = function(_, _, _, opts)
+  show = function(_, bufnr, _, opts)
+    if vim.bo.buftype ~= '' then
+      return
+    end
     -- Generally don't want it to open on every update
     opts.loclist.open = opts.loclist.open or false
-    local winid = vim.api.nvim_get_current_win()
-    vim.diagnostic.setloclist(opts.loclist)
-    vim.api.nvim_set_current_win(winid)
+    local winIds = vim.fn.win_findbuf(bufnr)
+    if #winIds > 0 then
+      local winid = vim.api.nvim_get_current_win()
+      vim.api.nvim_set_current_win(winIds[1])
+      vim.diagnostic.setloclist(opts.loclist)
+      vim.api.nvim_set_current_win(winid)
+    end
+  end,
+  ---@param bufnr integer
+  hide = function(_, bufnr)
+    if vim.bo.buftype ~= '' then
+      return
+    end
+    local winIds = vim.fn.win_findbuf(bufnr)
+    if #winIds > 0 then
+      local winid = vim.api.nvim_get_current_win()
+      vim.api.nvim_set_current_win(winIds[1])
+      vim.diagnostic.setloclist({ open = false })
+      vim.api.nvim_set_current_win(winid)
+    end
   end,
 }
 
