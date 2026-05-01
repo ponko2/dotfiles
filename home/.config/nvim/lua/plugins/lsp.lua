7879ed01fa31041a78bb55e032742510bcaeec30
Takahito Nakano <kano@ponko2.jp>
2026-05-02 06:59:23 +0900

    chore(neovim): wip

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -4,8 +4,14 @@ return {
   {
     'j-hui/fidget.nvim',
     cond = not vim.g.vscode,
-    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
-    config = true,
+    event = 'LspAttach',
+    opts = {
+      progress = {
+        ignore = {
+          'null-ls',
+        },
+      },
+    },
   },
   {
     'neovim/nvim-lspconfig',
@@ -36,6 +42,40 @@ return {
         'vue_ls',
       })
       vim.lsp.on_type_formatting.enable()
+    end,
+  },
+  {
+    'nvimtools/none-ls.nvim',
+    cond = not vim.g.vscode,
+    dependencies = {
+      'gbprod/none-ls-luacheck.nvim',
+      'nvim-lua/plenary.nvim',
+      'nvimtools/none-ls-extras.nvim',
+    },
+    event = 'LspAttach',
+    opts = function(_, opts)
+      local null_ls = require('null-ls')
+      opts.sources = {
+        -- Code Actions
+        null_ls.builtins.code_actions.textlint.with({
+          only_local = 'node_modules/.bin',
+        }),
+        -- Diagnostics
+        null_ls.builtins.diagnostics.golangci_lint,
+        require('none-ls-luacheck.diagnostics.luacheck'),
+        null_ls.builtins.diagnostics.textlint.with({
+          filetypes = { 'markdown', 'text' },
+          only_local = 'node_modules/.bin',
+        }),
+        -- Formatting
+        null_ls.builtins.formatting.prettier.with({
+          only_local = 'node_modules/.bin',
+        }),
+        null_ls.builtins.formatting.textlint.with({
+          filetypes = { 'markdown', 'text' },
+          only_local = 'node_modules/.bin',
+        }),
+      }
     end,
   },
 }
