b40c72e43dfd266e5aad823947064a9cbdad4af5
Takahito Nakano <kano@ponko2.jp>
2026-05-03 07:28:47 +0900

    chore(neovim): update lsp settings

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -105,55 +105,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
         buffer = ev.buf,
         callback = function()
-          ---@param action lsp.Command|lsp.CodeAction
-          ---@see https://github.com/neovim/neovim/blob/v0.12.2/runtime/lua/vim/lsp/buf.lua#L1247-L1260
-          local function apply_action(action)
-            if action.edit then
-              vim.lsp.util.apply_workspace_edit(action.edit, client.offset_encoding)
-            end
-            local a_cmd = action.command
-            if a_cmd then
-              local command = type(a_cmd) == 'table' and a_cmd or action
-              ---@cast command lsp.Command
-              client:exec_cmd(command, { bufnr = ev.buf })
-            end
-          end
-          ---@param kind string
-          local function apply_actions(kind)
-            local timeout_ms = 1000
-            ---@type lsp.CodeActionParams
-            local params = vim.lsp.util.make_range_params(0, client.offset_encoding)
-            params.context = { only = { kind }, diagnostics = {} }
-            local result =
-              client:request_sync('textDocument/codeAction', params, timeout_ms, ev.buf)
-            ---@type (lsp.Command|lsp.CodeAction)[]
-            local actions = result and result.result or {}
-            for _, action in ipairs(actions) do
-              if
-                not (action.edit and action.command)
-                and client:supports_method('codeAction/resolve')
-              then
-                local resolved, err =
-                  client:request_sync('codeAction/resolve', action, timeout_ms, ev.buf)
-                if err then
-                  vim.notify(err, vim.log.levels.ERROR)
-                elseif resolved and resolved.err then
-                  if action.edit or action.command then
-                    apply_action(action)
-                  else
-                    vim.notify(
-                      resolved.err.code .. ': ' .. resolved.err.message,
-                      vim.log.levels.ERROR
-                    )
-                  end
-                elseif resolved and resolved.result then
-                  apply_action(resolved.result)
-                end
-              else
-                apply_action(action)
-              end
-            end
-          end
+          local buf = require('ponko2.lsp.buf')
           local code_actions_on_save = {
             javascript = { 'source.fixAll', 'source.organizeImports' },
             javascriptreact = { 'source.fixAll', 'source.organizeImports' },
@@ -164,7 +116,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
           }
           local kinds = code_actions_on_save[vim.bo[ev.buf].filetype] or {}
           for _, kind in ipairs(kinds) do
-            apply_actions(kind)
+            buf.code_action({
+              diagnostics = {},
+              only = { kind },
+              triggerKind = vim.lsp.protocol.CodeActionTriggerKind.Automatic,
+            })
           end
         end,
       })
