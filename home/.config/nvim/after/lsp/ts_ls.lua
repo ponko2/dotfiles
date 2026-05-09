b6e534d2dc31390da752ac7e3887cf7afef872d1
Takahito Nakano <kano@ponko2.jp>
2026-05-09 18:00:00 +0900

    chore(neovim): update lsp settings

--- home/.config/nvim/after/lsp/ts_ls.lua
+++ home/.config/nvim/after/lsp/ts_ls.lua
@@ -13,7 +13,7 @@ return {
         name = '@vue/typescript-plugin',
         location = (function()
           local path = vim.fs.dirname(vim.fn.exepath('vue-language-server'))
-            .. '/../lib/language-tools/packages/typescript-plugin'
+            .. '/../lib/language-tools/packages/language-server'
           if vim.loop.fs_stat(path) then
             -- for Nix
             return path
@@ -22,9 +22,9 @@ return {
           return vim.env.HOMEBREW_PREFIX
             .. '/opt/vue-language-server/libexec/lib'
             .. '/node_modules/@vue/language-server'
-            .. '/node_modules/@vue/typescript-plugin'
         end)(),
-        languages = { 'javascript', 'typescript', 'vue' },
+        languages = { 'vue' },
+        configNamespace = 'typescript',
       },
     },
   },
