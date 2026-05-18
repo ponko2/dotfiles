e71b1d1a23120f8dac091a28616575cb3047a919
Takahito Nakano <kano@ponko2.jp>
2026-05-18 19:30:00 +0900

    chore(neovim): update lsp settings

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,3 @@
-local fs = require('ponko2.fs')
-
 return {
   {
     'j-hui/fidget.nvim',
@@ -11,19 +9,6 @@ return {
     'neovim/nvim-lspconfig',
     cond = not vim.g.vscode,
     config = function()
-      -- refs: https://github.com/neovim/nvim-lspconfig/blob/v2.9.0/lsp/eslint.lua#L114-L152
-      vim.lsp.config('eslint', {
-        root_dir = (function(root_dir)
-          ---@type fun(bufnr: integer, on_dir: fun(root_dir?: string))
-          return function(bufnr, on_dir)
-            root_dir(bufnr, function(root)
-              if vim.fn.executable(fs.resolve_node_cmd('eslint', root)) == 1 then
-                on_dir(root)
-              end
-            end)
-          end
-        end)(vim.lsp.config.eslint.root_dir),
-      })
       vim.lsp.enable({
         'copilot',
         'eslint',
