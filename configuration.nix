585bd16c86b578ebdb03361540c65ff15a5d7e38
Takahito Nakano <kano@ponko2.jp>
2026-09-09 00:00:00 +0900

    chore: migrate to OmniWM

--- configuration.nix
+++ configuration.nix
@@ -35,7 +35,7 @@
       "google-chrome"
       "karabiner-elements"
       "monitorcontrol"
-      "nikitabobko/tap/aerospace"
+      "omniwm"
       "snapzy"
       "zed"
     ];
@@ -55,11 +55,6 @@
     enableBashIntegration = false;
     enableFishIntegration = false;
     enableZshIntegration = false;
-    trust = {
-      casks = [
-        "nikitabobko/tap/aerospace"
-      ];
-    };
   };
   security.pam.services.sudo_local = {
     touchIdAuth = true;
