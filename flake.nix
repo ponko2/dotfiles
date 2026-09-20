a84675c0e4e506d0f8443bbb4b7472d7a53e5734
Takahito Nakano <kano@ponko2.jp>
2026-09-21 01:00:00 +0900

    chore(nix): update settings

--- flake.nix
+++ flake.nix
@@ -113,12 +113,6 @@
               ];
             };
             formatter = pkgs.nixfmt-tree;
-            packages = {
-              inherit (pkgs)
-                direnv
-                nix-direnv
-                ;
-            };
           };
         flake = {
           darwinConfigurations."ponko2" = withSystem "aarch64-darwin" (
